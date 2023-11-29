3bc9b1ccbb87c86fac33a0f1f445c74bbd788c30
abhishekpatra <abhishekpatra6129@gmail.com>
2023-11-29 16:08:50 +0530

    C1

---
+++ src/main/scala/lectures/basics/ValuesVariablesTypes.scala
@@ -0,0 +1,12 @@
+package lectures.basics
+
+object ValuesVariablesTypes extends App {
+
+  val x: Int=42
+  val y=45
+  println(x)
+  println(y)
+  // val is immutable i.e. we cannot reassign a value to a val
+  //compiler can infer datatypes of the val from the data assigned
+
+}
---
+++ src/main/scala/playground/playground.scala
@@ -0,0 +1,14 @@
+//Scala program to print a string
+//by using object oriented approach
+package playground
+
+object playground {
+
+  //main method
+  def main(args: Array[String]): Unit = {
+    //print function
+    println("learn scala")
+
+  }
+
+}
