2b1bc80a16499a54c5c3145d7cb6118ecdc76d2e
abhishekpatra <abhishekpatra6129@gmail.com>
2024-01-19 16:33:30 +0530

    intro

---
+++ src/main/scala/lectures/basics/intro.scala
@@ -0,0 +1,35 @@
+//Scala notes
+
+//intro.scala
+//basics
+//scalable programming language //it is a modern multiparadigm
+//programming language designed to express common programming
+//patterns in a concise ,elegant and type safe way //written by Martin Odersky at EPFL
+//1. Statically typed--
+//in statically typed language a variable has type and can hold only values of that type I/so if we need to declare a variable then we need to declare its type for every variable 
+//that means type error are caught by compliler and not during run time
+//scala is statically typed but it also uses type interfacing i.e it figures out the type 50 1€
+//2.runs on jvm full inter-op with java 
+//we can use any java code in scala and vice versa
+//3. object oriented
+//4. functional
+//5. dynamic features
+//6. scala blends object oriented and functional programming in a statically typed language
+//there are no null pointer exceptions in scala compared to java I/scala is not a interpreted language it is a compiled language
+
+//why use scala?
+//1. can be used as drop in replacement for java //2. Mixed scala/java projects
+//3.uses existing java libraries
+//4.uses existing java tools(Ant,Maven, Junit etc....)
+//5. Decent IDE support(netbeans, eclipse, intellij,vs)
+
+//SBT
+//sbt is scala build tool formerly simple build tool is an open siurce build tool for scala and java //projects similar to java's maven and ant
+//sbt is a modern build tool while it is written in scala and provides many scala convienences it is a //general purpose build tool
+//sbt is the de facto build tool in scala
+
+//why sbt?
+//1. native support for compiling scala code
+//2.uses apache ivy for dependency mangement--it also supports mave //3.only update on request model
+//4. full scala language support for creating tasks //5. support for mixed java/scala projects
+//6. launch REPL(read-eval-print Loop) in project context
